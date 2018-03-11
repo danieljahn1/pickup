4a7db26239d9fdada43cba16951ed60f3073f572
Daniel Ahn <danielahn@DAs-MacBook-Pro.attlocal.net>
2018-03-10 20:26:02 -0800

    initial commit with sign-up and sign-in components

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import Container from './components/container'
-import store from './redux/store/index'
+import store from './redux/store'
 
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux'
-import rootReducer from '../reducers/index'
+import rootReducer from '../redux/reducers'
 
 const store = createStore(rootReducer);
 
