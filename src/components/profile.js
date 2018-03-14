8aaf29cde86bead32f9e0e8e5175b25e19c3204b
Aanderson1986 <asquaredrealestate@gmail.com>
2018-03-13 17:37:20 -0700

    Create Event /w Redirect

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 class Profile extends React.Component {
     constructor(props) {
-        this.state ={}
+        this.state = {}
     }
-    return (){
+    return() {
         render(
-        <div>
-            <img src={props.image} id="profile-avatar" alt="Image for Profile"></img>
-            <h1>User Profile: {props.person.name}</h1>
-            <div className="Gender">{props.gender}</div>
-            <div className="Birthday">{props.birthday}</div>
-            <div className="Location">{props.location}</div>
-        </div>
+            <div>
+                <img src={props.image} id="profile-avatar" alt="Image for Profile"></img>
+                <h1>User Profile: {props.person.name}</h1>
+                <div class="form-control">
+                    <label class="header">Profile Photo:</label>
+
+                    <input id="image" type="file" name="profile_photo" placeholder="Photo" required="" capture></input>
+                </div>
+                <div className="Gender">{props.gender}</div>
+                <div className="Birthday">{props.birthday}</div>
+                <div className="Location">{props.location}</div>
+            </div>
         )
     }
 }
