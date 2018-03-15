bf03a64ff49bd919bbb4efa42e1abbbfcddcf4e1
e-masinas <emasinas@gmail.com>
2018-03-14 23:00:15 -0700

    Read in data from external json files

--- src/components/event-list.js
+++ src/components/event-list.js
@@ -5,6 +5,8 @@ import { Link, Switch, Route } from 'react-router-dom'
 
 import axios from 'axios'
 import {loadOtherEvents} from '../redux/actions'
+import {loadOtherUsers} from '../redux/actions'
+import {loadOtherParticipants} from '../redux/actions'
 import {updateEventsLoadFlag} from '../redux/actions'
 
 class EventList extends Component {
@@ -17,30 +19,58 @@ class EventList extends Component {
     }
 
     componentDidMount() {
-        // Get events from the local json file
+        // Get events from the local json file for more data
 
         if (this.props.loadedEventsJsonFile == false) {
-        axios.get('http://localhost:8080/data/events.json')
-            .then(response => {
-                // console.log(response.data.events);
+            axios.get('http://localhost:8080/data/events.json')
+                .then(response => {
+                    // console.log(response.data.events);
+                    // Add the events from the json file into the current array in redux state
+                    var loadEventsArr = this.props.events.slice();
 
-                // Add the events from the json file into the current array in redux state
-                var loadEventsArr = this.props.events.slice();
+                    // Loop through the json file and push the object into the array
+                    for (let i = 0; i < response.data.events.length; i++) {
+                        // console.log(response.data.events[i]);
+                        loadEventsArr.push(response.data.events[i]);
+                    }
+                    
+                    // console.log(loadEventsArr);
+                    
+                    this.props.loadEvents(loadEventsArr);
 
-                // Loop through the json file and push the object into the array
-                for (let i = 0; i < response.data.events.length; i++) {
-                    // console.log(response.data.events[i]);
-                    loadEventsArr.push(response.data.events[i]);
-                }
-                
-                // console.log(loadEventsArr);
-                
-                this.props.loadEvents(loadEventsArr);
+                    // set flag to true so the json data doesn't load more than once
+                    this.props.updateFlag(true);                    
+                })
 
-                // set flag to true so the json data doesn't load more than once
-                this.props.updateFlag(true);
-                
-            })
+            axios.get('http://localhost:8080/data/users.json')
+                .then(response => {
+                    // console.log(response.data.users);
+                    // Add the users from the json file into the current array in redux state
+                    var loadUsersArr = this.props.usersArr.slice();
+
+                    // Loop through the json file and push the object into the array
+                    for (let i = 0; i < response.data.users.length; i++) {
+                        loadUsersArr.push(response.data.users[i]);
+                    }
+                    // console.log(loadUsersArr);
+
+                    this.props.loadOtherUsers(loadUsersArr);                
+                })
+
+            axios.get('http://localhost:8080/data/participants.json')
+                .then(response => {
+                    // console.log(response.data.participants);
+                    // Add the participants from the json file into the current array in redux state
+                    var loadParticipantsArr = this.props.participants.slice();
+
+                    // Loop through the json file and push the object into the array
+                    for (let i = 0; i < response.data.participants.length; i++) {
+                        loadParticipantsArr.push(response.data.participants[i]);
+                    }
+                    // console.log(loadParticipantsArr);
+
+                    this.props.loadOtherParticipants(loadParticipantsArr);
+                })
         }
 
     }
@@ -136,11 +166,12 @@ class EventList extends Component {
             }            
         }        
 
-        // Set the event's status. If eventIDCounter <= max number of players, status is open
+        // Set the event's status. If max number of players is not zero, status is open
         let status = "Closed";
         let eventCopy =  this.props.events.filter(item => item.id == eventID );
         
-        if (eventIDCounter < eventCopy[0].maxPlayersNeeded) {
+        // if (eventIDCounter < eventCopy[0].maxPlayersNeeded) {
+        if (eventCopy[0].maxPlayersNeeded > 0) {
             // console.log("Number of participants: " + eventIDCounter);
             status = "Open";
         }        
@@ -157,14 +188,18 @@ const MapStateToProps = state => {
         events: state.events,
         participants: state.participants,
         loadedEventsJsonFile: state.loadedEventsJsonFile,
-        loggedInUser: state.loggedInUser
+        loggedInUser: state.loggedInUser,
+        usersArr: state.usersArr,
+        participants: state.participants
     }
 }
 
 const MapDispatchToProps = dispatch => {
     return {
         loadEvents: events => dispatch(loadOtherEvents(events)),
-        updateFlag: flag => dispatch(updateEventsLoadFlag(flag))
+        updateFlag: flag => dispatch(updateEventsLoadFlag(flag)),
+        loadOtherUsers: users => dispatch(loadOtherUsers(users)),
+        loadOtherParticipants: participants => dispatch(loadOtherParticipants(participants))
     }
 }
 
--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import axios from 'axios'
 
 class Home extends Component {
     constructor(props) {
